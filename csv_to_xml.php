5b39a2979f869b720c82d1289804db6ce4c864ca
Paul Davis <pauldavis4@gmail.com>
2019-02-19 13:11:08 +0000

    Outputs one csv per location

--- csv_to_xml.php
+++ csv_to_xml.php
@@ -2,7 +2,7 @@
 echo "working .. wait";
 ob_flush();
 flush();
-if (($handle = fopen("air_quality.csv", "r")) !== FALSE) {
+if (($handle = fopen("air_quality.csv", "r")) !== false) {
     
 	# define the tags - last col value in csv file is derived so ignore
 	$header = array('id', 'desc', 'date', 'time', 'nox', 'no', 'no2', 'lat', 'long');
@@ -19,29 +19,93 @@ if (($handle = fopen("air_quality.csv", "r")) !== FALSE) {
 	$row = 2;
 		
 	# start ##################################################
-	$out = '<records>';
-	
-	while (($data = fgetcsv($handle, 200, ",")) !== FALSE) {
-        
-        if ($data[0] == 10) {
-			$rec = '<row count="' . $count . '" id="' . $row . '">';
-		
-			for ($c=0; $c < $cols; $c++) {
-				$rec .= '<' . trim($header[$c]) . ' val="' . trim($data[$c]) . '"/>';
-			}
-			$rec .= '</row>';
-			$count++;
-			$out .= $rec;
+	$out['brislington'] = '<records>'; # 3
+	$out['fishponds'] = '<records>'; # 6
+	$out['parson_st'] = '<records>'; # 8
+	$out['rupert_st'] = '<records>'; # 9
+	$out['wells_rd'] = '<records>'; # 10
+	$out['newfoundland_way'] = '<records>'; # 11
+
+	while (($data = fgetcsv($handle, 200, ",")) !== false) {
+
+		switch ($data[0]) {
+			case 3:
+				$rec = '<row count="' . $count . '" id="' . $row . '">';
+
+				for ($c = 0; $c < $cols; $c++) {
+					$rec .= '<' . trim($header[$c]) . ' val="' . trim($data[$c]) . '"/>';
+				}
+				$rec .= '</row>';
+				$count++;
+				$out['brislington'] .= $rec;
+				break;
+			case 6:
+				$rec = '<row count="' . $count . '" id="' . $row . '">';
+
+				for ($c = 0; $c < $cols; $c++) {
+					$rec .= '<' . trim($header[$c]) . ' val="' . trim($data[$c]) . '"/>';
+				}
+				$rec .= '</row>';
+				$count++;
+				$out['fishponds'] .= $rec;
+				break;
+			case 8:
+				$rec = '<row count="' . $count . '" id="' . $row . '">';
+
+				for ($c = 0; $c < $cols; $c++) {
+					$rec .= '<' . trim($header[$c]) . ' val="' . trim($data[$c]) . '"/>';
+				}
+				$rec .= '</row>';
+				$count++;
+				$out['parson_st'] .= $rec;
+				break;
+			case 9:
+				$rec = '<row count="' . $count . '" id="' . $row . '">';
+
+				for ($c = 0; $c < $cols; $c++) {
+					$rec .= '<' . trim($header[$c]) . ' val="' . trim($data[$c]) . '"/>';
+				}
+				$rec .= '</row>';
+				$count++;
+				$out['rupert_st'] .= $rec;
+				break;
+			case 10:
+				$rec = '<row count="' . $count . '" id="' . $row . '">';
+
+				for ($c = 0; $c < $cols; $c++) {
+					$rec .= '<' . trim($header[$c]) . ' val="' . trim($data[$c]) . '"/>';
+				}
+				$rec .= '</row>';
+				$count++;
+				$out['wells_rd'] .= $rec;
+				break;
+			case 11:
+				$rec = '<row count="' . $count . '" id="' . $row . '">';
+
+				for ($c = 0; $c < $cols; $c++) {
+					$rec .= '<' . trim($header[$c]) . ' val="' . trim($data[$c]) . '"/>';
+				}
+				$rec .= '</row>';
+				$count++;
+				$out['newfoundland_way'] .= $rec;
+				break;
+			default:
+				echo "Your favorite color is neither red, blue, nor green!";
+
+				$row++;
 		}
-		$row++;
 	}
-	
-	$out .= '</records>';
-	# finish ##################################################
-	
+
+	foreach ($out as $x => $x_value) {
+	# close records tag
+		$out[$x] .= '</records>';
+
 	# write out file
-	file_put_contents('data/wells_rd.xml', $out);
-	
+		file_put_contents('data/' . $x . '.xml', $out[$x]);
+	}
+
+	# finish ##################################################
+
 	fclose($handle);
 }
 echo "....all done!";
